3e86f30af20c3d51df332793021f87ad0970025e
jason040510 <144149601+jason040510@users.noreply.github.com>
2024-09-25 21:46:54 -0400

    Set up Flask backend, added API endpoint for KMeans, and integrated frontend with Plotly

--- README.md
+++
@@ -1,1 +0,0 @@
-# jason123-assignment-2
--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
+import numpy as np
+from kmeans_scratch import KMeans  # Importing the KMeans class from our custom implementation
 
 app = Flask(__name__)
 
+# Route to serve the main webpage
 @app.route('/')
 def index():
-    return "Welcome to the KMeans Clustering App"
+    return render_template('index.html')  # This renders the HTML file which we will create
+
+# API endpoint to run the KMeans algorithm
+@app.route('/run_kmeans', methods=['POST'])
+def run_kmeans():
+    # Get the request data in JSON format
+    data = request.json
+    points = np.array(data['points'])  # Points for clustering
+    n_clusters = int(data['n_clusters'])  # Number of clusters (K)
+    init_method = data['init_method']  # Initialization method
+    
+    # Handle manual initialization if provided
+    if init_method == 'manual':
+        manual_centroids = np.array(data['manual_centroids'])
+        kmeans = KMeans(n_clusters=n_clusters, init=init_method, manual_centroids=manual_centroids)
+    else:
+        kmeans = KMeans(n_clusters=n_clusters, init=init_method)
+    
+    # Run the KMeans algorithm
+    centroids, labels = kmeans.fit(points)
+    
+    # Return the centroids and labels as JSON
+    return jsonify({'centroids': centroids.tolist(), 'labels': labels.tolist()})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
+
